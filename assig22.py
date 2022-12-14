9112cc8b1693ff688cd77d218bb7b02e6dcdb15d
Anushareddy1203 <118019188+Anushareddy1203@users.noreply.github.com>
2022-12-15 05:05:34 +0530

    Update assig22.py

--- assig22.py
+++ assig22.py
@@ -18,3 +18,23 @@ years = ["2005", "2008", "2010", "2012", "2015", "2018"]
 
 # read the csv file
 main_dataframe = pd.read_csv("C:/Users/anush/OneDrive/Desktop/World_bank_data.csv")
+
+def countries_years(file_name):
+    """This function takes a filename as argument, reads a dataframe in World bank format
+    and returns two dataframes:one with years as columns and one with countries as columns."""
+
+    # read the csv file
+    read_data = pd.read_csv(file_name)
+
+    # # Get unique values of Country Name and set as columns of the dataframe and transpose the dataframe
+    # country_columns = (
+    #     pd.DataFrame({"Country Name": read_data["Country Name"].unique()})
+    #     .set_index("Country Name")
+    #     .transpose()
+    # )
+
+    # # Get unique vales of Year set as index and transpose the dataframe.
+
+    # years_columns = (
+    #     pd.DataFrame({"Year": read_data.columns[4:]}).set_index("Year").transpose()
+    # )
